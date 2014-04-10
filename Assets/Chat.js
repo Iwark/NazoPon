c84881e3412aa2dda238b9245b9f7905eb6ff1f3
maimaiomt <maimaiask@gmail.com>
2014-04-10 23:14:43 +0900

    fixed conflicts

--- Assets/Chat.js
+++ Assets/Chat.js
@@ -18,24 +18,45 @@ function Start(){
 	style.fontSize = 11;
 	style.normal.textColor = Color.black;
 	
-/*	var x = new Texture2D(1, 1);
+	var x = new Texture2D(1, 1);
 	x.SetPixel(0,0,Color.white);
-	
 	style.normal.background = x;
-*/
-	user_name = "(ユーザー)";
+
+	user_name = "ユーザー:";
 }
 
 function OnGUI(){
 	var wx = Screen.width;
 	var wy = Screen.height;
-	chat = GUI.TextField(Rect(wx*1/9, wy*27/32, wx*7/9, wy*2/32), chat, 16);
+	chat = GUI.TextField(Rect(wx*1/9, wy*27/32, wx*7/9, wy*2/32), chat, 30);
 
 	if( GUI.Button( Rect(wx*6/9, wy*29/32, wx*2/9, wy*2/32), "送信") ){
-		addChat(user_name + chat);
+		var ChatText = user_name + chat;
+		var ChatLength : int = ChatText.Length;
+		Debug.Log(ChatLength);
+		if(ChatText.Length>18&& ChatText.Length<36){
+			//var chat0 = ChatText.Substring(0, 13);
+			var chat0 = ChatText.Substring(0, 18);
+			var chat1 = ChatText.Substring(18, ChatText.Length-18);
+			ChatText = chat0 + "\n" + chat1; 
+			//ChatText = ChatText.Replace(/(\d)(?=(\d\d\d)+$)/g, "$1,");
+		}
+		/*if(ChatText.Length>36){
+			//var chat0 = ChatText.Substring(0, 13);
+			var chat2 = ChatText.Substring(0, 18);
+		Debug.Log(chat2);
+			var chat3 = ChatText.Substring(18, ChatText.Length-18);
+					Debug.Log(chat3);
+			var chat4 = ChatText.Substring(ChatText.Length-17, ChatText.Length-36);
+						Debug.Log(chat4);
+		ChatText = chat2 + "\n" + chat3 + "\n" + chat4; 
+			//ChatText = ChatText.Replace(/(\d)(?=(\d\d\d)+$)/g, "$1,");
+		}*/
+		addChat(ChatText);
 	}
+	
 	GUILayout.BeginVertical(GUILayout.ExpandHeight(true));
-	GUI.Label( Rect(wx*1/9, wy*17/32, wx*5/9, wy/4), chatLabel, style);
+	GUI.Label( Rect(wx*1/9, wy*17/32, wx*7/9, wy*9/32), chatLabel, style);
     GUILayout.EndVertical();
 }
 function Update(){
@@ -43,8 +64,13 @@ function Update(){
 }
 
 function addChat(a:String){
-	for(var i=1; i<chats.length; i++){
+	/*for(var i=1; i<chats.length; i++){
 		chats[i-1] = chats[i];
 	}
 	chats[chats.length-1] = a;
+	*/
+	for(var i=chats.length-1; i>0; i--){
+		chats[i] = chats[i-1];
+	}
+	chats[0] = a;
 }
