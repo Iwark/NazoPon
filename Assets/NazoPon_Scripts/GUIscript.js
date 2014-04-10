15a3d5b9ee68b12dd22ec8418691f07738a1a739
Kakky <kakky@kakky.com>
2014-04-10 23:06:25 +0900

    b

--- Assets/NazoPon_Scripts/GUIscript.js
+++ Assets/NazoPon_Scripts/GUIscript.js
@@ -30,19 +30,19 @@ function OnGUI(){
 	
 	if( GUI.Button( Rect(wx*5/9, wy*27/32, wx*2/9, wy/8), clapButton) ){
 		addChat(user_name + ":\nありがとう！");
-		wss.SendMessage(null,0);
+		wss.SendMessage(null,"1");
 	}
 	if( GUI.Button( Rect(wx*7/9, wy*27/32, wx*2/9, wy/8), angerButton) ){
 		addChat(user_name + ":\nおいおい！");
-		wss.SendMessage(null,1);
+		wss.SendMessage(null,"2");
 	}
 	if( GUI.Button( Rect(wx*5/9, wy*23/32, wx*2/9, wy/8), IknowButton) ){
 		addChat(user_name + ":\n自信あり！");
-		wss.SendMessage(null,2);
+		wss.SendMessage(null,"3");
 	}
 	if( GUI.Button( Rect(wx*7/9, wy*23/32, wx*2/9, wy/8), IdontknowButton) ){
 		addChat(user_name + ":\n分からん…");
-		wss.SendMessage(null,3);
+		wss.SendMessage(null,"4");
 	}
 	
 	var origA = GUI.color.a;
@@ -55,7 +55,10 @@ function OnGUI(){
 }
 function Update(){
 	//会話ログを受信
-	
+	//var messages:List.< Dictionary.<String, Object> > = wss.messages as List.< Dictionary.<String, Object> >;
+	//var emotion:Dictionary.<String, Object> = messages[0] as Dictionary.<String, Object>;
+	//var emoNum:int = parseInt( emotion["emotion"] );
+	//print(emoNum);
 
 	chatLabel = chats.join("\n");
 }
