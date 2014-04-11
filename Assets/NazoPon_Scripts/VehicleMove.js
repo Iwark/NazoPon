f228b000a961937d0888cda382cd0bb61ec1689b
Kohei Iwasaki <iwasaki.kohei@donuts.ne.jp>
2014-04-11 10:40:37 +0900

    fix code

--- Assets/NazoPon_Scripts/VehicleMove.js
+++ Assets/NazoPon_Scripts/VehicleMove.js
@@ -3,7 +3,6 @@ private var speed = 50.0;
 private var curve_start_pos = 90.0;
 var initial_time = 2.0;
 var problem_time = 10.0;
-private var after_select_time = curve_start_pos/speed;
 private var curve_time = 51.8/speed;
 var loop_time = 18.0;
 private var rail_angle = 70.0;
@@ -20,7 +19,6 @@ private var curve_start_time = 0.0;
 private var curve_end_time = 0.0;
 
 private var PLAYER_MAX = 7;
-static var PROBLEM_MAX = 15;
 
 var going_migi:boolean;
 var migi_correct:boolean;
@@ -42,46 +40,29 @@ var trolley:Dictionary.<String, Object>;
 
 function Awake() {
 	wss = GameObject.Find("WebSocket").GetComponent(WebSocketScript);
-	trolley = Json.Deserialize(wss.trolley) as Dictionary.<String, Object>;
 	//users = trolley["users"];
 }
 
 function Start () {
-	//ここで、各問題の正答が左右どちらかを取得？
-	migi_correct = true;
+	
+	CreateMap();
 
 	going_migi = true;
 	selected = false;
 	gameover = false;
 
-	var stage_y = migi_correct ? 0 : -1000;
-	transform.position = new Vector3(0, stage_y, -speed*(problem_time+initial_time));
-
-	mainCamera = transform.Find("Main Camera").GetComponent("Camera");
-	subCameraA = GameObject.Find("SubCameraA").GetComponent("Camera");
-	subCameraB = GameObject.Find("SubCameraB").GetComponent("Camera");
-	mainCamera.enabled = true;
-	subCameraA.enabled = false;
-	subCameraB.enabled = false;
-
 	loop_count=0;
-	curve_start_time = initial_time + loop_count*loop_time + problem_time + after_select_time;
-	curve_end_time = curve_start_time + curve_time;
 
 	players = new Array(7);
 	addPlayer();
 	players[0].tag = "Player";
-	//addPlayer();
-	//addPlayer();
-	//addPlayer();
-	//addPlayer();
-	//addPlayer();
 	//addPlayer();
 	
 	scene_start_time = Time.time;
 }
 
 function addPlayer(){
+	
 	if(player_count < PLAYER_MAX){
 		players[player_count] = Instantiate(playerBoy, transform.position + Vector3(Random.Range(-1.5f,1.5f), 8, -(6.5+1.5*player_count)), Quaternion.identity);
 		players[player_count].transform.parent = transform;
@@ -119,25 +100,9 @@ function Update () {
 		if(is_seikai){
 			if(ctime >= initial_time + loop_time*(loop_count+1)){
 				loop_count += 1;
-				
-				if(loop_count < PROBLEM_MAX){
-
-					var stage_y = migi_correct ? 0 : -1000;
-	
-					transform.position = new Vector3(0, stage_y, -speed*problem_time);
-					transform.rotation = new Quaternion.Euler(0,0,0);
-					curve_start_time = initial_time + loop_count*loop_time + problem_time + after_select_time;
-					curve_end_time = curve_start_time + curve_time;
-					
-					selected = false;
-					is_seikai = false;
-					is_handan = false;
-					
-				}else{
-					gameover = true;			
-				}
+				CreateMap();
+				transform.position = new Vector3(0, transform.position.y, -speed*problem_time);
 			}
-		
 		//不正解時
 		}else{
 			var fall_start_time = initial_time + loop_time*(loop_count+1) - 2.5;
@@ -166,18 +131,42 @@ function Update () {
 		}
 		
 		transform.Translate(0, transY, transZ);
-	
 
 	//ゲームオーバー時
 	}else{
-		//Debug.Log("game over.");
+		Application.LoadLevel("Result");
 	}	
 }
 
-function updateTrolley(){
-	
+function CreateMap(){
+	trolley = Json.Deserialize(wss.trolley) as Dictionary.<String, Object>;
+	if(trolley["correct_way"] == 1) migi_correct = true;
+	else migi_correct = false;
+	Debug.Log("the correct way is right: "+migi_correct);
+
+	var stage_y = migi_correct ? 0 : -1000;
+	transform.position = new Vector3(0, stage_y, -speed*(problem_time+initial_time));
+	transform.rotation = new Quaternion.Euler(0,0,0);
+
+	mainCamera = transform.Find("Main Camera").GetComponent("Camera");
+	subCameraA = GameObject.Find("SubCameraA").GetComponent("Camera");
+	subCameraB = GameObject.Find("SubCameraB").GetComponent("Camera");
+	mainCamera.enabled = true;
+	subCameraA.enabled = false;
+	subCameraB.enabled = false;
+
+	var after_select_time = curve_start_pos/speed;
+	curve_start_time = initial_time + loop_count*loop_time + problem_time + after_select_time;
+	curve_end_time = curve_start_time + curve_time;
+
+	selected = false;
+	is_seikai = false;
+	is_handan = false;
+
 }
 
 function getTime(){
 	return Time.time - scene_start_time;
 }
+
+
