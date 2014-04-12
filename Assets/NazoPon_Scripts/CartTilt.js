9becd31879e04517124b88d9850709161bf0199b
maimaiomt <maimaiask@gmail.com>
2014-04-12 22:23:12 +0900

    nnnn

--- Assets/NazoPon_Scripts/CartTilt.js
+++ Assets/NazoPon_Scripts/CartTilt.js
@@ -9,6 +9,10 @@ private var initial_rotation;
 
 private var direction:float;
 
+//sound
+var cart : AudioSource;
+cart = GameObject.Find("Sound_Cart").GetComponent("AudioSource");
+
 function Start () {
 	vehicle = GameObject.Find("NazoPon");
 	script = vehicle.GetComponent(VehicleMove);
@@ -20,10 +24,12 @@ function Update () {
 	var time = script.getTime();
 	var c_s_time = script.curve_start_time;
 	var c_e_time = script.curve_end_time;
+	
 
 	if(c_e_time > c_s_time && c_s_time > 0.0 ){
 
 		if(c_s_time-1.0 < time && time <= c_s_time-0.9){
+	cart.Play();
 			direction = script.going_migi ? -1 : 1 ;
 			transform.Rotate( 0,0, direction * (tilt_angle/0.1) * Time.deltaTime);
 			script.is_tilted = true;
